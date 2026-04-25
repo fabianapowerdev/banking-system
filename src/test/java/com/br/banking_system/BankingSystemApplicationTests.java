4744048245dee1be3b105ee3630cf7ea0b6cd40e
fabianapowerdev <biazita.santo@hotmail.com>
2026-04-24 22:26:11 -0300

    atualizando BankingSystemApplicationTests.java

--- src/test/java/com/br/banking_system/BankingSystemApplicationTests.java
+++ src/test/java/com/br/banking_system/BankingSystemApplicationTests.java
@@ -2,12 +2,13 @@ package com.br.banking_system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class BankingSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
